f9ea79642e27e979732f4d076243ab851ff0beb2
squeemishly <squeemishly@gmail.com>
2017-05-27 14:18:55 -0600

    the admin can edit a stretch form the stretches index and a user cannot

--- spec/features/admin/edit_stretches_spec.rb
+++ spec/features/admin/edit_stretches_spec.rb
@@ -18,24 +18,24 @@ RSpec.describe "an admin can edit stretches" do
     expect(page).to have_content "not a stretch, but just as awesome"
   end
 
-  # it "when they access the edit form from the stretch index page" do
-  #   admin = create(:user, role: 1)
-  #   stretch = create(:stretch)
-  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-  #
-  #   visit stretches_path
-  #   click_on "Edit"
-  #
-  #   fill_in "stretch[name]", with: "High Fives!"
-  #   fill_in "stretch[description]", with: "not a stretch, but just as awesome"
-  #   click_on "Confirm Changes"
-  #
-  #   expect(current_path).to eq stretch_path(stretch)
-  #   expect(page).to have_content "High Fives!"
-  #   expect(page).to have_content "not a stretch, but just as awesome"
-  # end
+  it "when they access the edit form from the stretch index page" do
+    admin = create(:user, role: 1)
+    stretch = create(:stretch)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-  it "but a regular user cannot" do
+    visit stretches_path
+    click_on "Edit"
+
+    fill_in "stretch[name]", with: "High Fives!"
+    fill_in "stretch[description]", with: "not a stretch, but just as awesome"
+    click_on "Update Stretch"
+
+    expect(current_path).to eq stretch_path(stretch)
+    expect(page).to have_content "High Fives!"
+    expect(page).to have_content "not a stretch, but just as awesome"
+  end
+
+  it "but a regular user cannot edit a strecth" do
     user = create(:user)
     stretch = create(:stretch)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -45,5 +45,14 @@ RSpec.describe "an admin can edit stretches" do
 
     visit edit_admin_stretch_path(stretch)
     expect(page).to have_content "The page you were looking for doesn't exist."
+  end
+
+  it "when they access the edit form from the stretch index page" do
+    user = create(:user)
+    stretch = create(:stretch)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit stretches_path
+    expect(page).to_not have_content "Edit"
   end
 end
