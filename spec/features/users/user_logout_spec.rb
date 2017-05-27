b4a7186ecd5897e6c5c87458005e179e6515e44e
squeemishly <squeemishly@gmail.com>
2017-05-26 20:24:50 -0600

    added testing for user login and logout

--- spec/features/users/user_logout_spec.rb
+++ spec/features/users/user_logout_spec.rb
@@ -15,4 +15,16 @@ RSpec.describe "a user can logout" do
     click_on "Logout"
     expect(page).to_not have_content "Welcome, #{user.username}!"
   end
+
+  it "there isn't a logout button until a user is logged in" do
+    user = create(:user)
+
+    visit login_path
+    expect(page).to_not have_link "Logout"
+
+    fill_in "session[username]", with: user.username
+    fill_in "session[password]", with: "password"
+    click_on "Login"
+    expect(page).to have_link "Logout"
+  end
 end
