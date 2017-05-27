34b87b6ac7b7a81c6f32d766f7b5b53f7a0ec449
squeemishly <squeemishly@gmail.com>
2017-05-27 16:46:44 -0600

    admin can delete a stretch from the stretch index. users cannot delete a stretch at all

--- spec/features/admin/destroy_stretches_spec.rb
+++ spec/features/admin/destroy_stretches_spec.rb
@@ -13,35 +13,35 @@ RSpec.describe "an admin can destroy a stretch" do
     expect(page).to_not have_content stretch.name
   end
 
-  # it "from the stretch index" do
-  #   admin = create(:user, role: 1)
-  #   stretch = create(:stretch)
-  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-  #
-  #   visit stretches_path
-  #   expect(page).to have_content stretch.name
-  #   click_on "Delete"
-  #
-  #   expect(current_path).to eq stretches_path
-  #   expect(page).to_not have_content stretch.name
-  # end
-  #
-  # it "a user cannot destroy a stretch from the stretch show page" do
-  #   user = create(:user)
-  #   stretch = create(:stretch)
-  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-  #
-  #   visit stretch_path(stretch)
-  #   expect(page).to_not have_content "Delete"
-  # end
-  #
-  # it "a user cannot destroy a stretch from the stretch index" do
-  #   user = create(:user)
-  #   stretch = create(:stretch)
-  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-  #
-  #   visit stretches_path
-  #   expect(page).to have_content stretch.name
-  #   expect(page).to_not have_content "Delete"
-  # end
+  it "from the stretch index" do
+    admin = create(:user, role: 1)
+    stretch = create(:stretch)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit stretches_path
+    expect(page).to have_content stretch.name
+    click_on "Delete"
+
+    expect(current_path).to eq stretches_path
+    expect(page).to_not have_content stretch.name
+  end
+
+  it "a user cannot destroy a stretch from the stretch show page" do
+    user = create(:user)
+    stretch = create(:stretch)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit stretch_path(stretch)
+    expect(page).to_not have_content "Delete"
+  end
+
+  it "a user cannot destroy a stretch from the stretch index" do
+    user = create(:user)
+    stretch = create(:stretch)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit stretches_path
+    expect(page).to have_content stretch.name
+    expect(page).to_not have_content "Delete"
+  end
 end
