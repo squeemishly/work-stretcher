81c31e09412a5c46a35a614fae9c0efbc8af4798
squeemishly <squeemishly@gmail.com>
2017-05-28 11:33:21 -0600

    admin can select a body area for each stretch

--- app/controllers/admin/stretches_controller.rb
+++ app/controllers/admin/stretches_controller.rb
@@ -3,7 +3,6 @@ class Admin::StretchesController < ApplicationController
 
   def new
     @stretch = Stretch.new
-    @body_area = BodyArea.all
   end
 
   def create
--- spec/features/admin/stretches/create_stretches_spec.rb
+++ spec/features/admin/stretches/create_stretches_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "an admin can create stretches" do
   it "they can add a strech from the admin dashboard" do
     admin = create(:user, role: 1)
-    body_area = create(:body_area)
+    body_area = BodyArea.create(name: "Legs")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
     visit stretches_path
@@ -13,7 +13,7 @@ RSpec.describe "an admin can create stretches" do
     fill_in "stretch[name]", with: "Seated Piriformis Stretch"
     fill_in "stretch[description]", with: "Stretch that Piriformis!"
     fill_in "stretch[picture]", with: "http://www.athletico.com/wp-content/uploads/2016/06/seated-piriformis-chair-stretch-608x603.jpg"
-    select body_area.name, from: "stretch[body_area]"
+    find(:css, "#stretch_body_area_ids_#{body_area.id}[value='#{body_area.id}']").set(true)
     click_on "Create Stretch"
 
     expect(page).to have_content "Seated Piriformis Stretch has been created!"
