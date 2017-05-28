a8806c1c5af55bd6c9a93e316fbbb278bfdccdad
squeemishly <squeemishly@gmail.com>
2017-05-28 14:01:05 -0600

    changed body area search to a dropdown menu

--- app/controllers/body_areas_controller.rb
+++ app/controllers/body_areas_controller.rb
@@ -1,6 +1,6 @@
 class BodyAreasController < ApplicationController
   def show
-    params[:id] = BodyArea.find_by(name: params[:stretch_search]).id if params[:stretch_search]
+    params[:id] = params[:stretch_search][:body_area_id].to_i if params[:stretch_search]
     @body_area = BodyArea.find(params[:id])
     @stretches = @body_area.stretches.order(:name)
   end
